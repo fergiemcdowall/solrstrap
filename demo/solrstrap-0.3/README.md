207fc2bb01c040cfe50441ceef338b02e20628ca
Fergus McDowall <fergusmcdowall@Ferguss-MacBook-Air.local>
2013-03-17 19:55:54 +0100

    added the new demo of v0.3

---
+++ demo/solrstrap-0.3/README.md
@@ -0,0 +1,71 @@
+#Solrstrap
+
+[Solrstrap](http://fergiemcdowall.github.com/solrstrap/) is a Query-Result interface for Solr. [Solrstrap](http://fergiemcdowall.github.com/solrstrap/) is intended to be a starting point for those building web interfaces that talk to Solr, or a very lightweight admin tool for querying Solr in a Googleish fashion.
+
+**DEMO:** http://fergiemcdowall.github.com/solrstrap/demo/solrstrap-0.2/solrstrap.html
+
+**DOWNLOAD:** [Latest stable release- Solrstrap 0.2](https://github.com/fergiemcdowall/solrstrap/archive/v0.2.zip)
+
+**HOMEPAGE:** http://fergiemcdowall.github.com/solrstrap/
+
+**CLONE, FORK, GAZE AT CODE:** https://github.com/fergiemcdowall/solrstrap/
+
+#What does Solrstrap do?
+Solrstrap takes search queries and displays search results. It also features:
+* Instant search
+* Infinite scrolling
+* Facets
+* Restful interface (you can link directly to /solrstrap.html?q=doughnuts)
+* Functioning history
+
+#Solrstrap is probably the fastest available rendering engine for Solr.
+
+This is because it does everything in Javascript, CSS and HTML on the client side. JSON is shot back from the server and interpeted by the web browser.
+
+Solrstrap therefore requires much less server power and bandwidth than standard search-middleware applications.
+
+#Installation, How do I make it work?
+Optionally edit SERVERROOT in /js/solrstrap.js to point to the "select" endpoint of your solr instance, and HITTITLE/HITBODY to reference the appropriate fields in your index
+
+Click on /solrstrap.html.
+
+Thats it.
+
+#What is Solrstrap made of?
+Solrstrap is lovingly crafted from [Bootstrap](http://twitter.github.com/bootstrap/) and [Handlebars](http://handlebarsjs.com).
+
+#Strengths
+* Requires _only_ local installation- very easy to set up
+* Access to all Bootstrap functionality. Can be easily extended in a Bootstrappy way.
+* Blazing fast
+* Uses very little bandwidth
+
+#Weaknesses
+* Designed for "open" solr instances- needs clear access to /select/q=.
+* SEO basically non-existant
+* Will (probably) not work on truly ancient browsers (IE 7 and below)
+
+#Future releases
+* Some form of arrow key functionality...
+* Meaningful error messages and sanity checks...
+* Nicer code
+
+#License
+Copyright 2013 Fergus McDowall
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+#Contact
+Follow/contact me on Twitter [@fergiemcdowall](https://twitter.com/fergiemcdowall)
+
+I write articles about search engine technology here: http://blog.comperiosearch.com/blog/author/fmcdowall/
---
+++ demo/solrstrap-0.3/bootstrap/js/solrstrap.js
@@ -0,0 +1,178 @@
+
+
+//CONST- CHANGE ALL THESE TO TELL SOLRSTRAP ABOUT THE LOCATION AND STRUCTURE OF YOUR SOLR
+var SERVERROOT = 'http://evolvingweb.ca/solr/reuters/select/'; //SELECT endpoint
+var HITTITLE = 'title';                                        //Name of the title field- the heading of each hit
+var HITBODY = 'text';                                          //Name of the body field- the teaser text of each hit
+var HITSPERPAGE = 20;                                          //page size- hits per page
+var FACETS = ['topics','organisations'];                       //facet categories
+
+
+//when the page is loaded- do this
+  $(document).ready(function() {
+    $('#solrstrap-hits').append('<div offset="0"></div>');
+    $('#solrstrap-hits div[offset="0"]').loadSolrResults(getURLParam('q'), getURLParamArray('fq'), 0);
+    $('#solrstrap-searchbox').attr('value', getURLParam('q'));
+    $('#solrstrap-searchbox').focus();
+  });
+
+//when the searchbox is typed- do this
+  $('#solrstrap-searchbox').keyup(function() {
+    if ($(this).val().length > 3) {
+      $('#solrstrap-hits div[offset="0"]').loadSolrResults($(this).val(), getURLParamArray('fq'), 0);
+    }
+    else {
+      $('#solrstrap-hits').css({ opacity: 0.5 });
+    }
+  });
+
+  //jquery plugin allows resultsets to be painted onto any div.
+  (function( $ ){
+    $.fn.loadSolrResults = function(q, fq, offset) {
+      $(this).getSolrResults(q, fq, offset);
+    };
+  })( jQuery );
+
+
+  //jquery plugin allows autoloading of next results when scrolling.
+  (function( $ ){
+    $.fn.loadSolrResultsWhenVisible = function(q, fq, offset) {
+      elem = this;
+      $(window).scroll(function(event){
+        if (isScrolledIntoView(elem) && !$(elem).attr('loaded')) {
+          //dont instantsearch and autoload at the same time
+          if ($('#solrstrap-searchbox').val() != getURLParam('q')) {
+            window.location = 'solrstrap.html?q=' + $('#solrstrap-searchbox').val();
+          }
+          $(elem).attr('loaded', true);
+          $(elem).getSolrResults(q, fq, offset);
+          $(window).unbind('scroll');
+        }
+      });
+    };
+  })( jQuery );
+
+
+  //jquery plugin for takling to solr
+  (function( $ ){
+    $.fn.getSolrResults = function(q, fq, offset) {
+      var TEMPLATES = {
+        'hitTemplate':Handlebars.compile($("#hit-template").html()),
+        'summaryTemplate':Handlebars.compile($("#result-summary-template").html()),
+        'navTemplate':Handlebars.compile($("#nav-template").html()),
+        'chosenNavTemplate':Handlebars.compile($("#chosen-nav-template").html())
+      }
+      var rs = this;
+      $(rs).parent().css({ opacity: 0.5 });
+      $.getJSON(getSearchURL(q, offset),
+        function(result){
+          console.log(result);
+          //only redraw hits if there are new hits available
+          if (result.response.docs.length > 0) {
+            if (offset == 0) {
+              rs.empty();
+              //strapline that tells you how many hits you got
+              rs.append(TEMPLATES.summaryTemplate({totalresults: result.response.numFound, query: q}));
+              rs.siblings().remove();
+            }
+            //draw the individual hits
+            for (var i = 0; i < result.response.docs.length; i++) {
+              rs.append(TEMPLATES.hitTemplate({title: result.response.docs[i][HITTITLE], text: result.response.docs[i][HITBODY]}));
+            }
+            $(rs).parent().css({ opacity: 1 });
+            //if more results to come- set up the autoload div
+            if ((+HITSPERPAGE+offset) < +result.response.numFound) {
+              var nextDiv = document.createElement('div');
+              $(nextDiv).attr('offset', +HITSPERPAGE+offset);
+              rs.parent().append(nextDiv);
+              $(nextDiv).loadSolrResultsWhenVisible(q, fq, +HITSPERPAGE+offset);
+            }
+            //facets
+            $('#solrstrap-facets').empty();
+            //chosen facets
+            if (fq.length > 0) {
+              var chosenNavs = {};
+              for (var i = 0; i < fq.length; i++) {
+                chosenNavs[fq[i]] = ('?q=' + q + '&fq=' + fq.join('&fq=')).replace('&fq=' + fq[i], '');
+              }
+              $('#solrstrap-facets').append(TEMPLATES.chosenNavTemplate({navs: chosenNavs}));
+            }
+            //available facets
+            for (var k in result.facet_counts.facet_fields) {
+              if (result.facet_counts.facet_fields[k].length > 0) {
+                $('#solrstrap-facets').append(TEMPLATES.navTemplate({linkroot: window.location.pathname + '?q=' + q 
+                  + ((fq.length > 0) ? '&fq=' : '') + fq.join('&fq='),
+                  title: k, navs: makeNavsSensible(result.facet_counts.facet_fields[k])}));
+              }
+            }
+          }
+        });
+    };
+  })( jQuery );
+
+
+  //translates the ropey solr facet format to a more sensible map structure
+  function makeNavsSensible (navs) {
+    var newNav = {};
+    for (var i = 0; i < navs.length; i+=2) {
+      newNav[navs[i]] = navs[i + 1];
+    }
+    return newNav;
+  }
+
+  //utility function for grabbling URLs
+  function getURLParam(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.search);
+    if(results == null)
+      return "";
+    else
+      return decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+
+  //function to generate an array of URL parameters, where there are likely to be several params with the same key
+  function getURLParamArray(name) {
+    var paramArray = [];
+    var paramString = window.location.search;
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    while (regex.exec(paramString) != null) {
+      var results = regex.exec(paramString);
+      var paramValue = results[1].replace(/\+/g, " ");
+      paramString = paramString.replace("&" + name + "=" + paramValue, "");
+      paramArray.push(decodeURIComponent(paramValue));
+    }
+    return paramArray;
+  }
+
+  //utility function that checks to see if an element is onscreen
+  function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+
+  //generates a search URL from the CONSTants
+  function getSearchURL(q, offset) {
+    var URL = SERVERROOT
+    + '?json.wrf=?'
+    + '&rows=' + HITSPERPAGE
+    + '&wt=json'
+    + '&q=' + q
+    + '&start=' + offset;
+    if (FACETS.length > 0) {
+      URL += '&facet=true&facet.mincount=1&facet.limit=20';
+      for (var i = 0; i < FACETS.length; i++) {
+        URL += '&facet.field=' + FACETS[i];
+      }
+      for (var i = 0; i < getURLParamArray('fq').length; i++) {
+        URL += '&fq=' + getURLParamArray('fq')[i];
+      }
+    }
+    return URL;
+  }
